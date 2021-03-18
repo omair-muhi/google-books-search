71f0af935a2d52ae11905e0dc892c2e4aaae508f
Omair Muhi <omairmuhi@icloud.com>
2021-03-17 23:12:48 -0400

    Subject: Add new top-level container for Saved page Problem: NA Solution: NA Note: NA

---
+++ client/src/components/SavedContainer.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import Container from "./Container";
+import Row from "./Row";
+import Col from "./Col";
+import Card from "./Card";
+import BookDetail from "./BookDetail";
+import API from "../utils/API";
+
+class SavedContainer extends Component {
+    state = {
+        savedBooks: [],
+    };
+
+    componentDidMount() {
+        this.loadBooks();
+    }
+
+    loadBooks = () => {
+        API.getBooks()
+            .then(res => {
+                console.log("getBooks()");
+                console.log(res.data);
+                this.setState(state => {
+                    const savedBooks = [...res.data];
+                    return {
+                        savedBooks
+                    };
+                })
+            })
+            .catch(err => console.log(err));
+    };
+
+    // When delete button is clicked, delete MongoDB document
+    handleDeleteBooks = (id, event) => {
+        event.preventDefault();
+        console.log("Delete Book Handler called!", id);
+        API.deleteBook(id)
+            .then(res => this.loadBooks())
+            .catch(err => console.log(err));
+    };
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col size="md-8">
+                        {this.state.savedBooks.map(book =>
+                            <Card
+                                heading={book.title || "No saved books"}
+                                key={book._id}
+                                id={book._id}
+                                link={book.link}
+                                deleteBookHandler={this.handleDeleteBooks}
+                            >
+                                {book.title ? (
+                                    <BookDetail
+                                        title={book.title}
+                                        author={book.authors}
+                                        description={book.description}
+                                        thumbnail={book.image}
+                                        link={book.link}
+                                    />
+                                ) : (
+                                    <h3>No Results to Display</h3>
+                                )}
+                            </Card>
+                        )}
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+}
+
+export default SavedContainer;
--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,10 +1,11 @@
 // import React, { useState, useEffect } from "react";
 import React from "react";
+import SavedContainer from "../components/SavedContainer";
 
 function Saved() {
 
     return (
-        <h1>Saved Books</h1>
+        <SavedContainer />
     );
 }
 
