f448005dc939ec52b57ea7d0bb9322b0b316b17c
Omair Muhi <omairmuhi@icloud.com>
2021-03-17 23:52:36 -0400

    Deploying to Heroku

--- server.js
+++ server.js
@@ -22,8 +22,14 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+// use production or development DB
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    });
 
 app.listen(PORT, () => {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
